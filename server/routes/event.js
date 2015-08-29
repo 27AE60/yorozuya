192213a2554e0045310d82eb99866bc10770354e
ghome vic <green.rabi@hotmail.com>
2015-08-30 01:06:56 +0530

    POST /events

--- server/routes/event.js
+++ server/routes/event.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
-/* get events list */
-router.get('/', function(req, res) {
+var r = require('rethinkdb');
+
+function handleError(res, error) {
+  console.log('error', error);
+  return res.status(500).send({error: error.message});
+}
+
+/* get events list, filter by userId, startingAt and endingAt */
+router.get('/', function(req, res, next) {
+  var params = req.query;
+  console.log('params', params);
   return res.status(200).send("Yo Baby! On a Roll");
+});
+
+/* post an event */
+router.post('/', function(req, res, next) {
+  var params = req.body;
+  params.createdAt = r.now();
+  params.startingAt = new Date(params.startingAt);
+  params.endingAt = new Date(params.endingAt);
+
+  r.db('yorozuya').table('events').insert(params, {returnChanges: true}).run(req._rdbConn, function(error, result) {
+    if (error) {
+      handleError(res, error)
+    }
+    else if (result.inserted !== 1) {
+      handleError(res, new Error("Document was not inserted."))
+    }
+    else {
+      console.log('wtf', JSON.stringify(result.changes[0].new_val));
+      res.status(201).send(JSON.stringify(result.changes[0].new_val));
+    }
+    next();
+  });
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -9,6 +9,9 @@ yorozuya.use(bodyParser.json()); // accept JSON data, e.g. get JSON data from PO
 yorozuya.use(mdw.cors()); // enable Cross Origin Resource Sharing (CORS)
 yorozuya.use(require('morgan')('dev')); /* logger */
 
+// Start db connection
+yorozuya.use(mdw.createConnection);
+
 // Application routes
 yorozuya.use('/events', require('./routes/event.js'));
 
@@ -16,5 +19,8 @@ yorozuya.use('/events', require('./routes/event.js'));
 yorozuya.use(mdw.logErrors());
 yorozuya.use(mdw.clientErrorHandler());
 yorozuya.use(mdw.errorHandler());
+
+// Close db connection
+yorozuya.use(mdw.closeConnection);
 
 module.exports = yorozuya;
